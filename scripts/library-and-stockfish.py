a0505c19d07629735925489fc44621c2180ee002
SunshineKU <146216903+SunshineKU@users.noreply.github.com>
2024-05-21 21:09:33 +0300

    Another version added to library-and-stockfish.py

--- scripts/library-and-stockfish.py
+++ scripts/library-and-stockfish.py
@@ -1,4 +1,4 @@
-import chess.engine
+'''import chess.engine
 
 def get_depth(level):
     if level == "beginner":
@@ -44,6 +44,66 @@ def play_chess(level):
 
     print("Game Over")
     print("Result:", board.result())
+    engine.quit()
+
+if __name__ == "__main__":
+    level = input("Choose the level (beginner, intermediate, advanced, unbeatable): ").lower()
+    play_chess(level)
+    '''
+import chess.engine
+'''
+def get_depth(level):
+    if level == "beginner":
+        return 2
+    elif level == "intermediate":
+        return 4
+    elif level == "advanced":
+        return 6
+    elif level == "unbeatable":
+        return 20  # Or any high depth you desire
+    else:
+        return 4  # Default to intermediate
+        '''
+
+def play_chess(level):
+    # Start the Stockfish engine
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish\stockfish-windows-x86-64-avx2.exe")
+
+       # Start the Stockfish engine
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish\stockfish-windows-x86-64-avx2.exe")
+
+    # Start a new game
+    board = chess.Board()
+
+    player_turn = chess.WHITE if player_color == "white" else chess.BLACK     #player colour input form button
+
+    while not board.is_game_over():
+        #print(board)
+
+        if board.turn == player_turn:
+            # Person's move
+            while True:
+                try:
+                    person_move = input("Enter your move (e.g., e2e4): ") #change to input from camera
+                    move = chess.Move.from_uci(person_move)
+                    if move in board.legal_moves:
+                        board.push(move)
+                        break
+                    else:
+                        print("Invalid move, try again.")     
+                except ValueError:
+                    print("Invalid move format, try again.")
+
+        else:
+            # Stockfish's move
+            result1 = engine.play(board, chess.engine.Limit(depth=level))
+            best_move = result1.move
+            board.push(best_move)
+
+    global result
+    result=result=board.result()     
+    #print("Game Over")
+    #print("Result:", board.result())
     engine.quit()
 
 if __name__ == "__main__":
