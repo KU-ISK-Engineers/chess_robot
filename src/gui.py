1f5dd9cf8afcd068c9d9350b32827cec13d09c92
SunshineKU <146216903+SunshineKU@users.noreply.github.com>
2024-05-22 14:16:21 +0300

    Updated gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,19 +1,25 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import chess.engine
 
 robot_count = 0
+root = None
+game = None
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
 def update_robot_win_count():
     global robot_count
     read_robot_count_from_file()
     robot_count += 1
+    def write_robot_count_to_file():
+        with open("gui/robot_win_count.txt", "w") as file:
+            file.write(str(robot_count))
     write_robot_count_to_file()
+    def update_count_label():
+        count_label.config(text=str(robot_count))
     update_count_label()
-
-def write_robot_count_to_file():
-    global robot_count
-    with open("gui/robot_win_count.txt", "w") as file:
-        file.write(str(robot_count))
 
 def read_robot_count_from_file():
     global robot_count
@@ -24,87 +30,71 @@ def read_robot_count_from_file():
                 robot_count = int(content)
     except FileNotFoundError:
         robot_count = 0
+def place_image(path):
+    image = Image.open(path)
+    image = ImageTk.PhotoImage(image)
+    label = tk.Label(root, image=image, borderwidth=0)
+    x=(screen_width - image.width()) // 2
+    y=(screen_height - image.height()) // 2 - 200
+    label.image = image
+    label.place(x, y)
 
-def update_count_label():
-    count_label.config(text=str(robot_count))
+def win_lose_msg():
+    clear_widgets()
 
-def win_lose_msg(player_color, result, root):
-    for widget in root.winfo_children():
-        if widget not in logo_widgets:
-            clear_widget(widget)
-
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
 
     back = Image.open("images/back.png")
-    back = back.resize((200, 100), Image.Resampling.LANCZOS)
     back = ImageTk.PhotoImage(back)
-    back_button = tk.Button(root, image=back, command=lambda: levels_screen(root), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
+    back_button = tk.Button(root, image=back, command=lambda: levels_screen(), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     back_button.image = back
     back_button.place(x=1200, y=800)
 
-    game_over_image = Image.open("gui/images/game_over.png")
-    game_over_image = ImageTk.PhotoImage(game_over_image)
-    game_over_label = tk.Label(root, image=game_over_image, borderwidth=0)
-    game_over_label.image = game_over_image
-    game_over_label.place(x=(screen_width - game_over_image.width()) // 2, y=(screen_height - game_over_image.height()) // 2 - 200)
+
+    place_image("gui/images/game_over.png")
 
     def display_win_lose_messages():
         image_path = None
-        if player_color == "white":
-            if result == "1-0":
+        if game.board.perspective==chess.WHITE:
+            if game.check_game_over() == "1-0":
                 image_path = "images/you_won.png"
-            elif result == "0-1":
+            elif game.check_game_over() == "0-1":
                 image_path = "images/you_lose.png"
                 update_robot_win_count()
-            elif result == "1/2-1/2":
+            elif game.check_game_over() == "1/2-1/2":
                 image_path = "images/draw.png"
-        elif player_color == "black":
-            if result == "1-0":
+        elif game.board.perspective==chess.BLACK:
+            if game.check_game_over() == "1-0":
                 image_path = "images/you_lose.png"
                 update_robot_win_count()
-            elif result == "0-1":
+            elif game.check_game_over() == "0-1":
                 image_path = "images/you_won.png"
-            elif result == "1/2-1/2":
+            elif game.check_game_over() == "1/2-1/2":
                 image_path = "images/draw.png"
 
         if image_path:
-            win_lose_image = Image.open(image_path)
-            win_lose_image = ImageTk.PhotoImage(win_lose_image)
-            win_lose_label = tk.Label(root, image=win_lose_image, borderwidth=0)
-            win_lose_label.image = win_lose_image
-            win_lose_label.place(x=(screen_width - win_lose_image.width()) // 2, y=(screen_height - win_lose_image.height()) // 2 + 100)
+            place_image(image_path)
+    
 
     root.after(2000, display_win_lose_messages)
 
 def play_chess(level, player_color, root):
     result = "1-0"
-    #win_lose_msg(player_color, result, root)
+    win_lose_msg(player_color, result, root)
 
-def clear_widget(widget):
-    widget.destroy()
-
-def levels_screen(root):
+def clear_widgets():
     for widget in root.winfo_children():
         if widget not in logo_widgets:
-            clear_widget(widget)
+            widget.destroy()
 
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+def levels_screen():
+    clear_widgets()
 
     def select_level(level_value):
-        global level
-        level = level_value
-        for widget in root.winfo_children():
-            if widget not in logo_widgets:
-                clear_widget(widget)
-        color_screen(root)
+        game.depth = level_value
+        clear_widgets()
+        color_screen()
 
-    choose_level = Image.open("images/choose_level.png")
-    choose_level = ImageTk.PhotoImage(choose_level)
-    choose_label = tk.Label(root, image=choose_level, borderwidth=0)
-    choose_label.image = choose_level
-    choose_label.place(x=(screen_width - choose_level.width()) // 2, y=(screen_height - choose_level.height()) // 2 - 200)
+    place_image("images/choose_level.png")
 
     level1_img = Image.open("images/beginner.png")
     level2_img = Image.open("images/intermediate.png")
@@ -118,25 +108,26 @@ def levels_screen(root):
 
     level1_button = tk.Button(root, image=level1_img, command=lambda: select_level(2), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     level1_button.image = level1_img
-    level1_button.place(x=(screen_width - level1_img.width()) // 2, y=(screen_height - choose_level.height()) // 2 - 50)
+    level1_button.place(x=(screen_width - level1_img.width()) // 2, y=(screen_height - 110) // 2 - 50)
 
     level2_button = tk.Button(root, image=level2_img, command=lambda: select_level(4), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     level2_button.image = level2_img
-    level2_button.place(x=(screen_width - level2_img.width()) // 2, y=(screen_height - choose_level.height()) // 2 + 75)
+    level2_button.place(x=(screen_width - level2_img.width()) // 2, y=(screen_height - 110) // 2 + 75)
 
     level3_button = tk.Button(root, image=level3_img, command=lambda: select_level(6), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     level3_button.image = level3_img
-    level3_button.place(x=(screen_width - level3_img.width()) // 2, y=(screen_height - choose_level.height()) // 2 + 200)
+    level3_button.place(x=(screen_width - level3_img.width()) // 2, y=(screen_height - 110) // 2 + 200)
 
     level4_button = tk.Button(root, image=level4_img, command=lambda: select_level(20), borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     level4_button.image = level4_img
-    level4_button.place(x=(screen_width - level4_img.width()) // 2, y=(screen_height - choose_level.height()) // 2 + 325)
+    level4_button.place(x=(screen_width - level4_img.width()) // 2, y=(screen_height - 110) // 2 + 325)
 
 def receive_move():
+    
     update_turn("user")
 
 def send_move(w, update_turn_func):
-    clear_widget(w)
+    w.destroy()
     update_turn_func("robot")
 
 def update_turn(active, robot_label, user_label, robot_turn_active, robot_turn_inactive, your_turn_active, your_turn_inactive, finished_button):
@@ -145,18 +136,19 @@ def update_turn(active, robot_label, user_label, robot_turn_active, robot_turn_i
         robot_label.image = robot_turn_active
         user_label.config(image=your_turn_inactive)
         user_label.image = your_turn_inactive
+        game.player=ROBOT
+
     elif active == "user":
         robot_label.config(image=robot_turn_inactive)
         robot_label.image = robot_turn_inactive
         user_label.config(image=your_turn_active)
         user_label.image = your_turn_active
         screen_width = root.winfo_screenwidth()
+        game.player=HUMAN
         finished_button.place(x=(screen_width - finished_button.winfo_width()) // 2, y=650)
 
-def game_screen(root):
-    for widget in root.winfo_children():
-        if widget not in logo_widgets:
-            clear_widget(widget)
+def game_screen():
+    clear_widgets()
 
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -208,23 +200,22 @@ def game_screen(root):
     root.after(6000, lambda: update_turn("user", robot_label, user_label, robot_turn_active, robot_turn_inactive, your_turn_active, your_turn_inactive, finished_button))
 
 def assign_color(selected_color):
-    global player_color
-    player_color = selected_color
-    start_button(root)
+    #global player_color
+    #player_color = selected_color
+    if selected_color=="white":
+        game.board.perspective=chess.WHITE
+    elif selected_color=="black":
+        game.board.perspective=chess.BLACK
 
-def color_screen(root):
-    for widget in root.winfo_children():
-        if widget not in logo_widgets:
-            clear_widget(widget)
+    new_board = BoardWithOffsets(perspective=game.)
+    game.reset_board(new_board, move_pieces)
+    start_button()
 
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+def color_screen():
+    
+    clear_widgets()
 
-    choose_color = Image.open("images/choose_color.png")
-    choose_color = ImageTk.PhotoImage(choose_color)
-    choose_label1 = tk.Label(root, image=choose_color, borderwidth=0)
-    choose_label1.image = choose_color
-    choose_label1.place(x=(screen_width - choose_color.width()) // 2, y=(screen_height - choose_color.height()) // 2 - 200)
+    place_image("images/choose_color.png")
 
     color1 = Image.open("images/white.png")
     color1 = ImageTk.PhotoImage(color1)
@@ -239,13 +230,11 @@ def color_screen(root):
     button2.place(x=(screen_width - color2.width()) // 2, y=(screen_height - color2.height()) // 2 + 200)
 
 def start_game():
-    for widget in root.winfo_children():
-        if widget not in logo_widgets:
-            clear_widget(widget)
-    game_screen(root)
-    play_chess(level, player_color, root=root)
+    clear_widgets()
+    game_screen()
+    #play_chess(level, player_color, root=root)
 
-def add_logos(root):
+def add_logos():
     global logo_widgets, count_label
     logo_widgets = []
 
@@ -272,8 +261,6 @@ def add_logos(root):
     image3 = ImageTk.PhotoImage(image3)
     killcount = ImageTk.PhotoImage(killcount)
 
-    screen_width = root.winfo_screenwidth()
-
     total_width = image1.width() + image2.width() + image3.width()
     spacing = (screen_width - total_width) // 5
 
@@ -282,40 +269,29 @@ def add_logos(root):
     x3 = x2 + image2.width() + spacing
     x4 = 30
 
-    label1 = tk.Label(root, image=image1, borderwidth=0, bg="#FFFFFF")
-    label1.image = image1
-    label1.place(x=x1, y=0)
-    logo_widgets.append(label1)
-
-    label2 = tk.Label(root, image=image2, borderwidth=0, bg="#FFFFFF")
-    label2.image = image2
-    label2.place(x=x2, y=0)
-    logo_widgets.append(label2)
-
-    label3 = tk.Label(root, image=image3, borderwidth=0, bg="#FFFFFF")
-    label3.image = image3
-    label3.place(x=x3, y=0)
-    logo_widgets.append(label3)
-
-    killcount_label = tk.Label(root, image=killcount, borderwidth=0, bg="#FFFFFF")
-    killcount_label.image = killcount
-    killcount_label.place(x=x4, y=150)
-    logo_widgets.append(killcount_label)
+    def labels(img, x, y):
+        label1 = tk.Label(root, image=img, borderwidth=0, bg="#FFFFFF")
+        label1.image = image1
+        label1.place(x, y)
+        logo_widgets.append(label1)
+    
+    labels(image1, x1, 0)
+    labels(image2, x2, 0)
+    labels(image3, x3, 0)
+    labels(killcount, x4, 150)
 
     count_label = tk.Label(root, text=str(robot_count), bg="#FFFFFF", font=("Arial", 30), fg="black")
     count_label.place(x=x4 + killcount.width() // 2 - 20, y=230)
     logo_widgets.append(count_label)
 
-def create_empty_frame(root):
+def create_empty_frame():
     frame = tk.Frame(root, bg="#FFFFFF")
     frame.pack(fill=tk.BOTH, expand=True)
 
-def start_button(root):
+def start_button():
     global button
 
-    for widget in root.winfo_children():
-        if widget not in logo_widgets:
-            clear_widget(widget)
+    clear_widgets()
 
     image = Image.open("images/start.png")
     image = ImageTk.PhotoImage(image)
@@ -323,16 +299,15 @@ def start_button(root):
     button = tk.Button(root, image=image, command=start_game, borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
     button.image = image
 
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
     x = (screen_width - image.width()) // 2
     y = (screen_height - image.height()) // 2
 
     button.place(x=x, y=y)
 
-def gui_main():
-    global root
+def gui_main(game_obj):
+    global root, game
+
+    game = game_obj
     root = tk.Tk()
     root.configure(bg="#FFFFFF")
 
