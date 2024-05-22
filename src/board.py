566d3035df26ba76a7a520ffe961109704be5bef
np <nerpoc42@gmail.com>
2024-05-22 13:22:32 +0100

    Modified code

--- src/board.py
+++ src/board.py
@@ -1,6 +1,4 @@
 import chess
-import numpy as np
-import chess
 from typing import Optional
 from collections import namedtuple
 
@@ -8,21 +6,21 @@ SquareOffset = namedtuple('Offset', ['x', 'y'])
 SQUARE_CENTER = SquareOffset(0, 0)
 
 class BoardWithOffsets:
-    def __init__(self, board: chess.Board = None, offsets: np.ndarray = None, perspective: chess.Color = chess.WHITE):
+    def __init__(self, board: chess.Board = None, offsets: list = None, perspective: chess.Color = chess.WHITE):
         if board is None:
             board = chess.Board()
         if offsets is None:
-            offsets = np.array([[SQUARE_CENTER for _ in range(8)] for _ in range(8)], dtype=object)
+            offsets = [[SQUARE_CENTER for _ in range(8)] for _ in range(8)]
         
         self.chess_board = board
         self.offsets = offsets
         self.perspective = perspective
 
     def offset(self, square: chess.Square) -> SquareOffset:
-        return self.offsets[chess.square_rank(square), chess.square_file(square)]
+        return self.offsets[chess.square_rank(square)][chess.square_file(square)]
     
     def set_offset(self, square: chess.Square, offset: SquareOffset):
-        self.offsets[chess.square_rank(square), chess.square_file(square)] = offset
+        self.offsets[chess.square_rank(square)][chess.square_file(square)] = offset
 
     def piece_at(self, square: chess.Square):
         return self.chess_board.piece_at(square)
--- src/movement.py
+++ src/movement.py
@@ -13,20 +13,20 @@ def reflect_move(board: BoardWithOffsets, move: chess.Move) -> int:
     response = communication.RESPONSE_SUCCESS
     from_square, to_square = (move.from_square, move.to_square)
 
-    if board.is_castling(move):
+    if board.chess_board.is_castling(move):
         rook_move = _castle_rook_move(board, move)
 
         response = move_piece(board, from_square, to_square, response)
         response = move_piece(board, rook_move.from_square, rook_move.to_square, response)
     else:  # Regular moves
-        if board.is_capture(move):
+        if board.chess_board.is_capture(move):
             captured_piece = board.piece_at(to_square)
             piece_type, color = (captured_piece.piece_type, captured_piece.color)
             off_board_place = communication.off_board_square(piece_type, color)
 
             # Remove captured piece
             response = move_piece(board, to_square, off_board_place, response)
-        if board.is_en_passant(move):
+        if board.chess_board.is_en_passant(move):
             captured_square = _en_passant_captured(move)
             captured_piece = board.piece_at(captured_square)
             piece_type, color = (captured_piece.piece_type, captured_piece.color)
@@ -306,7 +306,7 @@ def main():
 
     detection = CameraDetection(camera, model)
 
-    prev_board = chess.Board()
+    prev_board = BoardWithOffsets()
     print(prev_board)
 
     communication.setup_communication()
@@ -317,9 +317,9 @@ def main():
         visualise_chessboard(board)
 
         if prev_board:
-            move = identify_move(prev_board, board)
+            move = identify_move(prev_board.chess_board, board.chess_board)
 
-            if move not in prev_board.legal_moves:
+            if move not in prev_board.legal_moves():
                 pass
                 #print('Illegal move')
             else:
@@ -337,7 +337,7 @@ def main():
                     print('response fail')
                     time.sleep(5)
 
-def visualize_move_with_arrow(board, move, filename):
+def visualize_move_with_arrow(board: BoardWithOffsets, move: chess.Move, filename: str):
     """
     Visualize a move on the chessboard with an arrow from the starting square to the ending square.
     
@@ -347,7 +347,7 @@ def visualize_move_with_arrow(board, move, filename):
     arrow = (move.from_square, move.to_square)
     
     # Display the board with the arrow
-    svg_before = chess.svg.board(board=board, size=400, arrows=[arrow])
+    svg_before = chess.svg.board(board=board.chess_board, size=400, arrows=[arrow])
 
     # Save the SVG image to a file
     with open(filename, 'w') as f:
