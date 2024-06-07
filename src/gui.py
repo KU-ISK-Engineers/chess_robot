2e5328c407be982496f7ac6fcf503e56a1a090c5
sunshine <sauljan159@gmail.com>
2024-06-07 18:36:12 +0300

    updating rules (help_screen) and incorporating into gui.py

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 from .game import HUMAN, ROBOT
 import threading
 import chess
+from tkvideo import tkvideo
 
 robot_count = 0
 game_thread = None
@@ -34,6 +35,71 @@ def read_robot_count_from_file():
 def update_count_label():
     count_label.config(text=str(robot_count))
 
+    
+def help_screen():
+    global main_frame, left_frame
+    clear_screen()
+    
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    left_frame = tk.Frame(root, width=screen_width/3, background='white')
+    left_frame.pack(side="left", fill="y")
+    main_frame = tk.Frame(root,  background='white')
+    main_frame.pack(side="right", fill="both", expand=True)
+
+    # List of images for buttons and corresponding videos
+    button_images = ["images/pawn_button.png", "images/rook_button.png", "images/knight_button.png", "images/bishop_button.png", "images/queen_button.png", "images/king_button.png"]
+    video_files = ["images/pawn_moves.mp4", "images/rook_moves.mp4", "images/knight_moves.mp4", "images/bishop_moves.mp4", "images/queen_moves.mp4", "images/king_moves.mp4"]
+
+    # Create buttons with images in the left frame
+    for i, button_image_path in enumerate(button_images):
+        button_image = Image.open(button_image_path)
+        button_image = button_image.resize((200, 100), Image.Resampling.LANCZOS)
+        button_image = ImageTk.PhotoImage(button_image)
+        
+        button = tk.Button(left_frame, image=button_image, command=lambda i=i: show_video(video_files[i]),borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
+        button.image = button_image
+        button.pack(pady=10, anchor="center")
+
+
+def show_video(video_path):
+    global current_player
+
+    for widget in main_frame.winfo_children():
+        widget.destroy()
+
+    main_frame.update_idletasks()  # Ensure the frame has been fully updated
+    frame_width = main_frame.winfo_width()
+    video_size = int(frame_width * 0.5) 
+
+    video_label = tk.Label(main_frame, width=video_size, height=video_size)
+    video_label.place(relx=0.5, rely=0.5, anchor='center')
+    
+    current_player = tkvideo(video_path, video_label, loop=1, size=(video_size, video_size))
+    current_player.play()
+
+    back_button_image = Image.open("images/back.png")
+    back_button_image = back_button_image.resize((50, 50), Image.Resampling.LANCZOS)
+    back_button_image = ImageTk.PhotoImage(back_button_image)
+
+    # Back button
+    back_button_image = Image.open("images/back.png")
+    back_button_image = back_button_image.resize((100, 70), Image.Resampling.LANCZOS)
+    back_button_image = ImageTk.PhotoImage(back_button_image)
+    back_button = tk.Button(root, image=back_button_image, command=clear_screen, borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
+    back_button.image = back_button_image
+    back_button.place(relx=0.9, rely=0.9, anchor='center')
+
+def help_button():
+    help_button_image = Image.open("images/help.png")
+    help_button_image = help_button_image.resize((100, 75), Image.Resampling.LANCZOS)
+    help_button_image = ImageTk.PhotoImage(help_button_image)
+
+    help_button = tk.Button(root, image=help_button_image, command=help_screen, borderwidth=0, highlightthickness=0, relief='flat', bg="#FFFFFF")
+    help_button.image = help_button_image
+    
+    return help_button
 
 def clear_screen():
     for widget in root.winfo_children():
@@ -99,6 +165,10 @@ def background():
     count_label = tk.Label(root, text=str(robot_count), bg="#FFFFFF", font=("Arial", 30), fg="black")
     count_label.place(x=x4 + killcount.width() // 2 - 20, y=230)
     logo_widgets.append(count_label)
+
+    help=help_button()
+    help.place(relx=0.1, rely=0.45, anchor='center')
+    logo_widgets.append(help)
 
 #start_game: game_screen and a thread for game
 def start_button():
